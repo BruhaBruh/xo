daf08c3374ca2ddc784852548fa2da42f00354be
BruhaBruh <drugsho.jaker@gmail.com>
2024-05-03 23:10:06 +0500

    fix: socket url

--- apps/client/src/lib/socket/index.ts
+++ apps/client/src/lib/socket/index.ts
@@ -2,4 +2,8 @@
 
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.NEXT_PUBLIC_SERVER_URL!);
+export const socket = io(
+  process.env.NEXT_PUBLIC_SERVER_URL!.startsWith('https://')
+    ? `wss://${process.env.NEXT_PUBLIC_SERVER_URL!.substring(8)}`
+    : `ws://${process.env.NEXT_PUBLIC_SERVER_URL!.substring(7)}`
+);
